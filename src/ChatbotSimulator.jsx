4bb9ddfccb788e0099a5074e26878fa3fb746201
cutiefunny <cutiefunny@gmail.com>
2025-08-12 13:26:56 +0900

    re

--- src/ChatbotSimulator.jsx
+++ src/ChatbotSimulator.jsx
@@ -314,11 +314,12 @@ function ChatbotSimulator({ nodes, edges, isVisible }) {
           }
           break;
         case 'date':
-          const today = new Date();
-          const year = today.getFullYear();
-          const month = String(today.getMonth() + 1).padStart(2, '0');
-          const day = String(today.getDate()).padStart(2, '0');
-          defaultData[element.name] = `${year}-${month}-${day}`;
+          // const today = new Date();
+          // const year = today.getFullYear();
+          // const month = String(today.getMonth() + 1).padStart(2, '0');
+          // const day = String(today.getDate()).padStart(2, '0');
+          // defaultData[element.name] = `${year}-${month}-${day}`;
+          defaultData[element.name] = '2025-08-20';
           break;
         case 'checkbox':
           if (element.options?.length > 0) {
